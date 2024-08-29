630e28b4d9d8bf23c58a7d6255babb1eee298b48
Sameea-Student <144456655+Sameea-Student@users.noreply.github.com>
2024-08-29 22:15:44 +0500

    task adding

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const TodoModel = require('./Models/Todo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/local')
+app.get('/get', (req, res) => {
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.post('/add', (req, res) => {
+    console.log(req.body)
+    const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001, () => {
+    console.log("Server is running")
+
+})
